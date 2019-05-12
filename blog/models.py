f0104c66aba32048d128a7caa9ae43f22641718f
wkake8888 <wkake0568@gmail.com>
2019-05-12 12:49:03 +0800

    Commit Blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    contents = models.CharField(max_length=1000)
+    summary = models.CharField(max_length=200)
