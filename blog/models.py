2a1ea6dc8d80d347c490200030d6985182b9fd10
wkake8888 <wkake0568@gmail.com>
2019-05-16 16:25:26 +0800

    Added bootstrap and jobs are ahowing up

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Blog(models.Model):
-    title = models.CharField(max_length=20)
-    pub_date = models.DateField()
-    body = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=20, default=None)
+    pub_date = models.DateField(default=None)
+    body = models.CharField(max_length=200, default=None)
+    image = models.ImageField(upload_to='images/', default=None)
