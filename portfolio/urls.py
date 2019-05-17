f58b15cdf80c43b2f3396c435803b9ab706b7000
wkake8888 <wkake0568@gmail.com>
2019-05-17 09:37:44 +0800

    Change

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 import jobs.views
@@ -24,5 +24,6 @@ import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', jobs.views.home, name="home")
+    path('', jobs.views.home, name="home"),
+    path('blog/', include('blog.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
