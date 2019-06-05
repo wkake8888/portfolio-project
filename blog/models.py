de05795497843af58781837dee05f51ef878a87b
wkake8888 <wkake0568@gmail.com>
2019-06-05 16:22:04 +0800

    'polished'

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=20, default=None)
     pub_date = models.DateField(default=None)
-    body = models.CharField(max_length=200, default=None)
+    body = models.CharField(max_length=5000, default=None)
     image = models.ImageField(upload_to='images/', default=None)
 
     def summary(self):
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'portfolio/static/')
+    os.path.join(BASE_DIR, 'portfolio/static')
 ]
 
 
